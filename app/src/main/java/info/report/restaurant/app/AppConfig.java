fa74e32b469c3ce877928ad0d43ce023af0cd6d2
MHbappy <mhbappy18@gmail.com>
2017-06-16 15:33:07 +0600

    First commit

---
+++ app/src/main/java/info/report/restaurant/app/AppConfig.java
@@ -0,0 +1,9 @@
+package info.report.restaurant.app;
+
+public class AppConfig {
+	// Server user login url
+	public static String URL_LOGIN = "http://192.168.32.71/android_login_api/login.php";
+
+	// Server user register url
+	public static String URL_REGISTER = "http://192.168.32.71/android_login_api/register.php";
+}
